a286b179115bc17039fd43aca9002bca41352c5e
MrsBrandon <36216329+MrsBrandon@users.noreply.github.com>
2019-07-14 09:54:40 +0800

    Add files via upload
    Request accomplished
    Match App: Arduino

---
+++ sketch_jul10b.ino
@@ -0,0 +1,18 @@
+#include <Microduino_ColorLED.h>
+
+ColorLED strip = ColorLED(1, 6);
+
+void setup()
+{
+  strip.begin();
+}
+
+void loop()
+{
+  if (!digitalRead(5)) {
+    strip.setPixelColor(6-1, 255,0,0);
+    strip.show();
+
+  }
+
+}
